9e57df6ef636874c52f2698db203215ea661decc
Bruno <bruno.8716943@edu.univali.br>
2026-09-23 21:46:56 -0300

    tudo bugado

--- aula01/app.py
+++ aula01/app.py
@@ -2,6 +2,16 @@
 """
 
 import streamlit as st
+import dados
 
+st.set_page_config(layout="wide")
 st.title("📚 Dashboard de Livros")
-st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+
+livros = dados.ler_livros()
+qtd_livros = len(livros)
+st.metric("Total de Livros", qtd_livros)
+
+preco_medio = dados.calcular_preco_medio(livros)
+st.metric("Preço médio", f"{preco_medio:.2f}")
+
+st.dataframe(livros)
